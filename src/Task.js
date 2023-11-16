d8835c479d96d2fb6083ee4714a9ddb4511e7a26
ardasem <73624089+ardasem@users.noreply.github.com>
2023-11-16 18:37:04 +0300

    date-fns added

--- src/Task.js
+++ src/Task.js
@@ -1,18 +1,30 @@
 import React from "react";
+import { formatDistanceToNow } from "date-fns";
 
 const Task = ({ taskObj, onComplete }) => {
-
+ const dateArr = taskObj.deadline.split('-');
+ console.log(dateArr)
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
+      <div className="deadline">
+        son teslim:{" "}
+        <span>
+          {formatDistanceToNow(new Date(dateArr[0], dateArr[2], dateArr[1]), { addSuffix: true })
+          }
+        </span>
+      </div>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
-          <span className="pill" key={p}>{p}</span>
+          <span className="pill" key={p}>
+            {p}
+          </span>
         ))}
       </div>
-      {onComplete && <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>}
+      {onComplete && (
+        <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>
+      )}
     </div>
   );
 };
--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form";
 import { nanoid } from "nanoid";
 import { toast } from 'react-toastify';
 
+
 export default function TaskHookForm({ kisiler, submitFn }) {
   const {
     register,
